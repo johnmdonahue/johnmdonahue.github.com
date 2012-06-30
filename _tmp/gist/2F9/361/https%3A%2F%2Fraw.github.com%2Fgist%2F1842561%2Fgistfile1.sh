b8c5e5b9566f9217c6c62a4454b20daad3a59800
John Donahue <john.m.donahue@gmail.com>
2012-06-30 00:35:39 -0700

    going static

--- _tmp/gist/2F9/361/https%3A%2F%2Fraw.github.com%2Fgist%2F1842561%2Fgistfile1.sh
+++
@@ -1,12 +0,0 @@
-o: ActiveSupport::Cache::Entry	:@compressedF:@expires_in0:@created_atf1340954129.684814:@value"I"$ sudo apt-get update
-$ sudo apt-get install git-core curl build-essential openssl libssl-dev
-
-# node
-$ git clone git://github.com/joyent/node.git && cd node
-$ git checkout v0.6.17
-$ ./configure
-$ make
-$ sudo make install
-
-# npm
-$ curl http://npmjs.org/install.sh | sudo sh:ET
